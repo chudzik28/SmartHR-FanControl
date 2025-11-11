1b7557cdfea9748f76e09d798ac5358ae72612e6
Marcin Chudzkiewicz <chudzik28@gmail.com>
2025-11-11 19:43:23 +0100

    Fixed Bug with BT status in release version:second try

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.chudzikiewicz.smarthrfancontrol"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
