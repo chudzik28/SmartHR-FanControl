abd646630237fc9e39c445c7db658fe45949c55e
Marcin Chudzkiewicz <chudzik28@gmail.com>
2025-11-11 20:54:20 +0100

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.chudzikiewicz.smarthrfancontrol"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
